db3bd3a6b1560c1409dcee35ecc7b532d7d3f661
SpizhovyiMaxDev <max.spizhovyi@gmail.com>
2023-11-20 00:00:33 -0800

    Part 10

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import './index.css';
+// import App from './App';
+import StarRating from './StarRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating = {5} messages = {['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}/>
+    <StarRating size = {24} maxRating = {10} color = 'red' className = "test"/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
